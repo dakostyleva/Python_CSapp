65eb97c41596259a3ae1a32e9a23c909bd1b02be
dakostyleva <dashatkacheva@gmail.com>
2021-06-07 19:44:12 +0300

    To create pull request

--- task_2.py
+++
@@ -1,7 +0,0 @@
-list = ('class', 'function', 'method')
-
-for word in list:
-    word_bytes = bytes(word, 'utf-8')
-    print(word_bytes)
-    print(type(word_bytes))
-    print(f'длина равна {len(word_bytes)}')
--- task_4.py
+++
@@ -1,8 +0,0 @@
-
-list = ['разработка', 'администрирование', 'protocol', 'standard']
-
-for word in list:
-    enc_word = word.encode('utf-8')
-    print(enc_word)
-    dec_word = enc_word.decode('utf-8')
-    print(dec_word)
